93a1a98dc889532a65d577471eacaa0d9a85bfee
quindio <npalaciosr@uqvirtual.edu.co>
2019-08-01 10:31:30 -0500

    intento mil de subir archivos

---
+++ home.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { PreguntasPage } from '../app/preguntas/preguntas.page';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {
+
+  }
+
+}
